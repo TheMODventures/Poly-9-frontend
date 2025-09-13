2e767e1a556ca05f2a548212c4d500822ac5f6f9
SameerAther <unknowncoder769@gmail.com>
2025-09-13 20:27:26 +0500

    feat(auth): add login and register pages with forms feat(private): implement chat, context file, layout, home, profile, and settings pages

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,15 +1,14 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Sidebar from '@/components/shared/sidebar'
-import Navbar from '@/components/shared/navbar'
+import React from "react";
+import Sidebar from "@/components/shared/sidebar";
+import Navbar from "@/components/shared/navbar";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
-
   return (
     <div className="min-h-screen">
       <Navbar />
-      <div className="flex" style={{ height: 'calc(100vh - 50px)' }}>
+      <div className="flex" style={{ height: "calc(100vh - 50px)" }}>
         <Sidebar />
         <div className="flex-1 flex items-center justify-center p-4 sm:p-6 md:p-8 overflow-auto relative  bg-gradient-to-t from-blue-50 via-purple-50 to-white">
           <div className="absolute inset-0 flex items-center justify-center z-20 bg-[#00000021]/13">
@@ -18,7 +17,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default RootLayout
+export default RootLayout;
