842646578bff15e35edce1f292493598b1d3e314
SameerAther <unknowncoder769@gmail.com>
2025-09-25 23:49:45 +0500

    refactor(navbar, links): remove notifications functionality and clean up imports

--- utils/links.tsx
+++ utils/links.tsx
@@ -1,22 +1,23 @@
-import { LuLayoutGrid } from "react-icons/lu"
-import { IoPersonOutline, IoSettingsOutline } from "react-icons/io5"
-import { BiSolidMessageRounded } from "react-icons/bi"
-import { RiSofaLine } from "react-icons/ri"
-import { LuSofa } from "react-icons/lu"
-import { IoBedOutline } from "react-icons/io5"
-import { PiApplePodcastsLogoThin } from "react-icons/pi"
+import { LuLayoutGrid } from "react-icons/lu";
+import { IoPersonOutline, IoSettingsOutline } from "react-icons/io5";
+import { RiSofaLine } from "react-icons/ri";
+import { LuSofa } from "react-icons/lu";
+import { IoBedOutline } from "react-icons/io5";
+import { PiApplePodcastsLogoThin } from "react-icons/pi";
 
-
-export  const links = [
-    { href: "/", icon: LuLayoutGrid },
-    { href: "/profile", icon: IoPersonOutline },
-    { href: "/chat", icon: BiSolidMessageRounded },
-    { href: "/settings", icon: IoSettingsOutline },
-  ]
+export const links = [
+  { href: "/", icon: LuLayoutGrid },
+  { href: "/profile", icon: IoPersonOutline },
+  { href: "/settings", icon: IoSettingsOutline },
+];
 
 export const categories = [
-    { name: "Armchair", active: true, icon: <RiSofaLine className="w-4 h-4" /> },
-    { name: "Sofa", active: false, icon: <LuSofa className="w-4 h-4" /> },
-    { name: "Bed", active: false, icon: <IoBedOutline className="w-4 h-4" /> },
-    { name: "Light", active: false, icon: <PiApplePodcastsLogoThin className="w-4 h-4" /> },
-  ]
+  { name: "Armchair", active: true, icon: <RiSofaLine className="w-4 h-4" /> },
+  { name: "Sofa", active: false, icon: <LuSofa className="w-4 h-4" /> },
+  { name: "Bed", active: false, icon: <IoBedOutline className="w-4 h-4" /> },
+  {
+    name: "Light",
+    active: false,
+    icon: <PiApplePodcastsLogoThin className="w-4 h-4" />,
+  },
+];
